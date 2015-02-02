04469176fcbf6e2e3274df3bb913471f366ab9db
Martin Stroet <martin.stroet@gmail.com>
2015-02-02 11:30:16 +1000

    add cbp-init.pdb which contains graphene layer only; partially completed deposition code

--- CBPDeposition.py
+++ CBPDeposition.py
@@ -3,6 +3,7 @@ import os
 from os.path import join
 from traceback import format_exc
 import pmx
+import random
 
 GMX_PATH = "/home/uqbcaron/PROGRAMMING_PROJECTS/CPP/gromacs-4.0.7/build/bin/"
 OUT_PDB_FILE = "cbp{cbpNumber}-end.pdb"
@@ -18,6 +19,7 @@ MDRUN_TEMPLATE = "mpirun -np 8 --bind-to-none {GMX_PATH}mdrun_mpi_d -np 8 -pd -s
                                                                                                                                "GMX_PATH":"{GMX_PATH}"}) 
 
 INSERT_DISTANCE = 40.0 #angstrom 
+CONTACT_TOLERANCE = 4.0
 
 ROOT_DIR = os.path.abspath("./")
 
@@ -60,21 +62,41 @@ class CBPDeposition(object):
             errFile.close()
     
     def getInsertHeight(self):
-        return max([a.x[2] for a in self.model.atoms]) + INSERT_DISTANCE
+        return  self.maxZHeight() + INSERT_DISTANCE
     
-    
+    def maxZHeight(self):
+        return max([a.x[2] for a in self.model.atoms])
+        
+    def getRandomPosXY(self):
+        x, y = map(lambda x:[v*10 for v in x], self.model.box[:2])
+        # take the 0th and 1st element from x and y respectively as these must be rectangular boxes
+        return random.uniform(0.0, x[0]), random.uniform(0.0, y[1])
+
+    def hasReachedLayer(self):
+        lastResID = self.model.residues[-1].id
+        lastRes = self.model.residue(lastResID)
+        maxLayerHeight = max([a.x[2] for a in self.model.atoms if a not in lastRes.atoms])
+        return any([a.x[2] < maxLayerHeight + CONTACT_TOLERANCE for a in lastRes])
 
 def main():
-    cBPDeposition = CBPDeposition("300") 
+    cBPDeposition = CBPDeposition("300")
+    
+    print cBPDeposition.hasReachedLayer()
+    
     insertHeight = cBPDeposition.getInsertHeight()
     
+    xPos, yPos = cBPDeposition.getRandomPosXY()
+    
     cBPModel_000 = pmx.Model(join(ROOT_DIR, CBP_PDB_000))
-    cBPModel_000.translate([0,0,insertHeight])
+    cBPModel_000.translate([xPos, yPos, insertHeight])
+    cBPModel_000.random_rotation()
     
     newCBP = int(cBPDeposition.cbpNumber) + 1
     cBPDeposition.model.insert_residue(newCBP, cBPModel_000.residues[0], " ")
     
     cBPDeposition.model.write(IN_PDB_FILE.format(**{"cbpNumber":newCBP}), "{0} CBP molecules".format(newCBP), 0)
+    
+    print cBPDeposition.hasReachedLayer()
     
     #runSystem("300")
 
