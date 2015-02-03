5da2fffeb299f936e48471085770d6d7d54df751
Martin Stroet <martin.stroet@gmail.com>
2015-02-03 15:53:27 +1000

    working version with mpi

--- CBPDeposition.py
+++ CBPDeposition.py
@@ -6,104 +6,171 @@ import pmx
 import random
 import logging
 import math
+import shutil
 
-RUN_WITH_MPI = False
+RUN_WITH_MPI = True
+VERBOCITY = logging.INFO
+VERBOCITY = logging.DEBUG
 
-GMX_PATH = "/home/uqbcaron/PROGRAMMING_PROJECTS/CPP/gromacs-4.0.7/build/bin/"
-OUT_PDB_FILE = "cbp{cbpNumber}-end.gro"
-IN_PDB_FILE = "cbp{cbpNumber}-init.gro"
+MAX_CORES = 8
 
-CBP_PDB_000 = "templates/cbp.pdb"
-GRAPHENE_PDB = "templates/graphene.pdb"
-ITP = "templates/cbp-massmod.itp"
-
-INSERT_DISTANCE = 15.0 #angstrom 
+INSERT_DISTANCE = 20.0 #angstrom 
 CONTACT_TOLERANCE = 4.0
 DRIFT_TIME = 4.0 #ps
+
 DRIFT_VEL = INSERT_DISTANCE*1e-1/DRIFT_TIME
+DRIFT_VEL_REALISTIC = 0.08
+DRIFT_VEL = DRIFT_VEL_REALISTIC
 
 
-SETUP_TEMPLATE = "make {cbpNumber}"
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-GPP_TEMPLATE = "{GMX_PATH}grompp_d -f run.mdp -c {pdbTemplate} -p topo.top -o md.tpr".format(**{"pdbTemplate":IN_PDB_FILE,
+GMX_PATH = "/home/uqbcaron/PROGRAMMING_PROJECTS/CPP/gromacs-4.0.7/build/bin/"
+OUT_STRUCT_FILE = "cbp{moleculeNumber}-end.gro"
+IN_STRUCT_FILE = "cbp{moleculeNumber}-init.gro"
+
+TEMPLATE_DIR = join(PROJECT_DIR, "templates")
+
+CBP_PDB_000 = join(TEMPLATE_DIR, "cbp.pdb")
+
+GRAPHENE_PDB = join(TEMPLATE_DIR, "graphene.pdb")
+ITP = join(TEMPLATE_DIR, "cbp-massmod.itp")
+
+SETUP_TEMPLATE = "make {moleculeNumber} template_dir={templatePath}"
+
+GPP_TEMPLATE = "{GMX_PATH}grompp_d -f run.mdp -c {pdbTemplate} -p topo.top -o md.tpr".format(**{"pdbTemplate":IN_STRUCT_FILE,
                                                                                                 "GMX_PATH":"{GMX_PATH}"})
 
-MPI_ADDITION = "mpirun -np 16 --bind-to-none "
+MPI_ADDITION = "mpirun -np {0} --bind-to-none "
+
+MDRUN = "mdrun_mpi_d" if RUN_WITH_MPI else "mdrun_d"
 
 RERUN_FLAG = "-cpi md.cpt -append"
 
-MDRUN_TEMPLATE = "{mpiRun}{GMX_PATH}mdrun_d -pd -s md.tpr -deffnm md -c {pdbTemplate} {reRunFlag}".format(**{"pdbTemplate":OUT_PDB_FILE,
+
+MDRUN_TEMPLATE = "{mpiRun}{GMX_PATH}{mdrun} -pd -s md.tpr -deffnm md -c {pdbTemplate} {reRunFlag}".format(**{"pdbTemplate":OUT_STRUCT_FILE,
+                                                                                                             "mdrun":MDRUN,
                                                                                                              "GMX_PATH":    "{GMX_PATH}",
                                                                                                              "reRunFlag":   "{reRunFlag}",
                                                                                                              "mpiRun":      "{mpiRun}"}) 
 
-RERUN_TEMPLATE = "{GMX_PATH}tpbconv_d -s md.tpr -extend {extendTime} -o md.tpr".format(**{"GMX_PATH":"{GMX_PATH}",
+RERUN_SETUP_TEMPLATE = "{GMX_PATH}tpbconv_d -s md.tpr -extend {extendTime} -o md.tpr".format(**{"GMX_PATH":"{GMX_PATH}",
                                                                                         "extendTime":DRIFT_TIME}) 
 
 TEMPERATURE = 300 #k
 
 K_B = 0.00831451 #kJ / (mol K)
 
-ROOT_DIR = os.path.abspath("./")
+MIXTURE = {CBP_PDB_000:21,
+           }
 
-
-class CBPDeposition(object):
+class Deposition(object):
     
-    def __init__(self, cbpNumber):
+    def __init__(self, moleculeNumber, rootdir):
         
-        self.cbpNumber = cbpNumber
+        logging.basicConfig(level=VERBOCITY, format='%(asctime)s - [%(levelname)s] - %(message)s  -->  (%(module)s.%(funcName)s: %(lineno)d)', datefmt='%d-%m-%Y %H:%M:%S')
         
-        if cbpNumber == 0:
-            pdbPath = join(ROOT_DIR, GRAPHENE_PDB)
+        self.moleculeNumber = moleculeNumber
+        self.rootdir = os.path.abspath(rootdir)
+        
+        if not os.path.exists(self.rootdir):    
+            os.makedirs(self.rootdir)
+        
+        # copy makefile to rootdir
+        makefilePath = join(rootdir, "Makefile")
+        if not os.path.exists(makefilePath):
+            shutil.copy(join(PROJECT_DIR, "Makefile"), makefilePath)
+        
+        if self.moleculeNumber == 0:
+            configurationPath = join(PROJECT_DIR, GRAPHENE_PDB)
         else:
-            pdbPath = join(ROOT_DIR, str(self.cbpNumber), OUT_PDB_FILE.format(**{"cbpNumber":self.cbpNumber}))
-            
-        self.model = pmx.Model(pdbPath)
+            configurationPath = join(self.rootdir, str(self.moleculeNumber), OUT_STRUCT_FILE.format(**{"moleculeNumber":self.moleculeNumber}))
+        
+        self.model = pmx.Model(configurationPath)
         self.model.nm2a()
         
         self.log = "out.log"
         self.err = "out.err"
         
         
-    def runSystem(self, rerun):
+    def updateModel(self, configurationPath):
+        logging.info("Updating model with new configuration")
+        #self.model.updateGRO(configurationPath)
+        self.model = pmx.Model(configurationPath)
+        self.model.nm2a()
+        
+    def runSystem(self, rerun=False):
         
         reRunFlag = RERUN_FLAG if rerun else ""
-        mpiRun = MPI_ADDITION if RUN_WITH_MPI else ""
         
+        if RUN_WITH_MPI:
+            mpiRun = MPI_ADDITION.format(MAX_CORES) if self.moleculeNumber > MAX_CORES else MPI_ADDITION.format(self.moleculeNumber) 
+        else:
+            mpiRun = ""
+            
         inserts = {"GMX_PATH":   GMX_PATH,
-                    "cbpNumber": self.cbpNumber,
+                    "moleculeNumber": self.moleculeNumber,
                     "reRunFlag": reRunFlag,
                     "mpiRun": mpiRun}
-        if rerun:
-            self.run(RERUN_TEMPLATE, inserts, join(ROOT_DIR, str(self.cbpNumber)), self.log, self.err)
         
-        self.run(MDRUN_TEMPLATE, inserts, join(ROOT_DIR, str(self.cbpNumber)), self.log, self.err)
+        if rerun:
+            # run rerun setup script
+            self.runTPBConf(inserts)
+        else:
+            # run grompp 
+            self.runGPP(inserts)
+        
+        # run the md
+        self.run(MDRUN_TEMPLATE, inserts)
+        
+        configurationPath = join(self.rootdir, str(self.moleculeNumber), OUT_STRUCT_FILE.format(**{"moleculeNumber":self.moleculeNumber})) 
+        
+        if not os.path.exists(configurationPath):
+            logging.error("MD run did not produce expected output file")
+            errorLog = open(join(self.rootdir, str(self.moleculeNumber), self.err)).readlines()
+            for i, l in enumerate(errorLog):
+                if "fatal error" in l.lower():
+                    logging.error("Error from GROMACS:\n{0}\n".format("".join(errorLog[i:i+7])))
+                    break
+        
+        # now update model after run
+        self.updateModel(configurationPath)
     
-    def runGPP(self):
-        inserts = {"GMX_PATH":  GMX_PATH,
-                   "cbpNumber": self.cbpNumber}
-        self.run(GPP_TEMPLATE, inserts, join(ROOT_DIR, str(self.cbpNumber)), self.log, self.err)
+    def runTPBConf(self, inserts):
+        self.run(RERUN_SETUP_TEMPLATE, inserts)
+    
+    def runGPP(self, inserts):
+        
+        self.run(GPP_TEMPLATE, inserts)
         
     def runSetup(self):
         inserts = {"GMX_PATH": GMX_PATH,
-                        "cbpNumber": self.cbpNumber}
-        self.run(SETUP_TEMPLATE, inserts, ROOT_DIR, self.log, self.err)
+                    "moleculeNumber": self.moleculeNumber,
+                    "templatePath": TEMPLATE_DIR}
+        
+        self.run(SETUP_TEMPLATE, inserts, workdir=self.rootdir)
     
-    def run(self, argString, inserts, workDir, logPath, errPath):
+    def run(self, argString, inserts, workdir=None):
         
         argString = argString.format(**inserts)
         argList = argString.split()
+        if not workdir:
+            workdir = join(self.rootdir, str(self.moleculeNumber))
         
-        logFile = open(join(workDir, logPath),"a")
-        errFile = open(join(workDir, errPath),"a")
+        logFile = open(join(workdir, self.log),"a")
+        errFile = open(join(workdir, self.err),"a")
         try:
-            #print argString
-            subprocess.Popen(argList, cwd=workDir, stdout=logFile, stderr=errFile).wait()
+            logging.debug("Running from: '{0}'".format(workdir))
+            logging.debug("Running command: '{0}'".format(argString))
+            subprocess.Popen(argList, cwd=workdir, stdout=logFile, stderr=errFile).wait()
         except:
             print "Subprocess terminated with error: \n{0}\n\n{1}".format(argString, format_exc())
         finally: 
             logFile.close()
             errFile.close()
+        
+        
+        
     
     def getInsertHeight(self):
         return  self.maxZHeight() + INSERT_DISTANCE
@@ -117,9 +184,6 @@ class CBPDeposition(object):
         return random.uniform(0.0, x[0]), random.uniform(0.0, y[1])
 
     def hasReachedLayer(self):
-        # pdbPath sould become self.pdbPath method
-        pdbPath = join(ROOT_DIR, str(self.cbpNumber), OUT_PDB_FILE.format(**{"cbpNumber":self.cbpNumber}))
-        self.model.updateGRO(pdbPath)
         lastResID = self.model.residues[-1].id
         lastRes = self.model.residue(lastResID)
         maxLayerHeight = max([a.x[2] for a in self.model.atoms if a not in lastRes.atoms])
@@ -133,6 +197,36 @@ class CBPDeposition(object):
             atom.v[0] = random.gauss(0.0, sigma)
             atom.v[1] = random.gauss(0.0, sigma)
             atom.v[2] = -abs(random.gauss(DRIFT_VEL, sigma))
+            
+        
+    def sampleMixture(self):
+        if len(MIXTURE) == 1:
+            return join(PROJECT_DIR, MIXTURE.keys()[0])
+        
+                    
+    def getNextMolecule(self):
+        nextMolPath = self.sampleMixture()
+        nextMolecule = pmx.Model(nextMolPath)
+        nextMolecule.nm2a()
+        
+        with open(join(PROJECT_DIR, ITP),"r") as fh:
+            cbpITPString = fh.read()
+        massDict = getMassDict(cbpITPString)
+        
+        # set masses to 1.0 to avoid warning
+        for atom in nextMolecule.atoms:
+            atom.m = massDict[atom.name]
+        
+        insertHeight = self.getInsertHeight()
+        xPos, yPos = self.getRandomPosXY()
+            
+        nextMolecule.translate([xPos, yPos, insertHeight])
+        nextMolecule.random_rotation()
+        return nextMolecule
+        
+    def writeInitConfiguration(self):
+        updatedPDBPath = join(self.rootdir, str(self.moleculeNumber), IN_STRUCT_FILE.format(**{"moleculeNumber":self.moleculeNumber}))
+        self.model.write(updatedPDBPath, "{0} CBP molecules".format(self.moleculeNumber), 0)
         
 def getMassDict(itpString):
     itpString = itpString.split("[ atoms ]")[1].split("[ bonds ]")[0]
@@ -143,53 +237,36 @@ def getMassDict(itpString):
         massDict[line.split()[4]] = float(line.split()[7])
     return massDict
 
-def main(cbpCount, cbpMax):
+def runDeposition(cbpCount, cbpMax, rootdir):
     
-    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - [%(levelname)s] - %(message)s  -->  (%(module)s.%(funcName)s: %(lineno)d)', datefmt='%d-%m-%Y %H:%M:%S')
-    cBPDeposition = CBPDeposition(cbpCount)
+    deposition = Deposition(cbpCount, rootdir)
     
     logging.info("Running deposition with drift velocity of {0:.3f} nm/ps".format(DRIFT_VEL))
-    while cBPDeposition.cbpNumber < cbpMax:
+    while deposition.moleculeNumber < cbpMax:
         # increment cbp number
-        cBPDeposition.cbpNumber += 1
+        deposition.moleculeNumber += 1
         
         # create run directory and run setup make file
-        cBPDeposition.runSetup()
-        insertHeight = cBPDeposition.getInsertHeight()
-        xPos, yPos = cBPDeposition.getRandomPosXY()
+        deposition.runSetup()
         
-        cBPModel_000 = pmx.Model(join(ROOT_DIR, CBP_PDB_000))
-        cBPModel_000.nm2a()
-        
-        with open(join(ROOT_DIR, ITP),"r") as fh:
-            cbpITPString = fh.read()
-        massDict = getMassDict(cbpITPString)
-        
-        # set masses to 1.0 to avoid warning
-        for atom in cBPModel_000.atoms:
-            atom.m = massDict[atom.name]
-            
-        cBPModel_000.translate([xPos, yPos, insertHeight])
-        cBPModel_000.random_rotation()
-        
-        
-        cBPDeposition.model.insert_residue(cBPDeposition.cbpNumber, cBPModel_000.residues[0], " ")
-        
-        cBPDeposition.genInitialVelocitiesLastResidue()
+        # get the next molecule and insert it into the deposition model with random position, orientation and velocity
+        nextMolecule = deposition.getNextMolecule()
+        deposition.model.insert_residue(deposition.moleculeNumber, nextMolecule.residues[0], " ")
+        deposition.genInitialVelocitiesLastResidue()
      
         # write updated model to run directory
-        updatedPDBPath = join(ROOT_DIR, str(cBPDeposition.cbpNumber), IN_PDB_FILE.format(**{"cbpNumber":cBPDeposition.cbpNumber}))
-        cBPDeposition.model.write(updatedPDBPath, "{0} CBP molecules".format(cBPDeposition.cbpNumber), 0)
-        
-        # run grompp
-        cBPDeposition.runGPP()
+        deposition.writeInitConfiguration()
         
         # Do first Run
-        logging.info("Running with {0} CBP molecules".format(cBPDeposition.cbpNumber))
-        cBPDeposition.runSystem(False)
-        while not cBPDeposition.hasReachedLayer():
-            logging.info("Rerunning with {0} CBP molecules due to molecule not reaching layer".format(cBPDeposition.cbpNumber))
-            cBPDeposition.runSystem(True)
+        logging.info("Running with {0} CBP molecules".format(deposition.moleculeNumber))
+        deposition.runSystem()
         
-    logging.info("Finished deposition of {0} CBP molecules".format(cBPDeposition.cbpNumber))
-main(0, 4)
+        while not deposition.hasReachedLayer():
+            logging.info("Rerunning with {0} CBP molecules due to molecule not reaching layer".format(deposition.moleculeNumber))
+            deposition.runSystem(rerun=True)
+        
+    logging.info("Finished deposition of {0} CBP molecules".format(deposition.moleculeNumber))
+    
+    
+runDeposition(45, 301, os.path.abspath("./dep60"))
+#runDeposition(108, 111, os.path.abspath("./firstRun110Mols"))
--- Makefile
+++ Makefile
@@ -1,20 +1,20 @@
 SHELL=/bin/bash
 
 %/topo.top: 
-	n=$(shell echo $@ | egrep -o '[0-9]+') erb templates/topo.top > $@
+	n=$(shell echo $@ | egrep -o '[0-9]+') erb $(template_dir)/topo.top > $@
 
 %/run.pbs: 
-	n=$(shell echo $@ | egrep -o '[0-9]+') erb templates/run.pbs > $@
+	n=$(shell echo $@ | egrep -o '[0-9]+') erb $(template_dir)/run.pbs > $@
 	chmod +x $@
 
 %/run.mdp:
-	n=$(shell echo $@ | egrep -o '[0-9]+') erb templates/depo-50ps-run.mdp > $@
+	n=$(shell echo $@ | egrep -o '[0-9]+') erb $(template_dir)/depo-50ps-run.mdp > $@
 	
 %/gph80OK.itp:
-	cp templates/gph80OK.itp $@
+	cp $(template_dir)/gph80OK.itp $@
 
 %/cbp-massmod.itp:
-	cp templates/cbp-massmod.itp $@
+	cp $(template_dir)/cbp-massmod.itp $@
 
 %:
 	[[ ! -d $@ ]] && mkdir $@
