df398e0856eb8e0b651c6fea7cb09032dc828e13
Tom Lee <contact@tomlee.com.au>
2018-12-31 14:34:25 +1000

    Fix bug in submit_wiener.sh

--- submit_wiener.sh
+++ submit_wiener.sh
@@ -5,27 +5,26 @@ set -o pipefail  # These lines cause bash to quit if any command has an error
 set -o verbose
 set -o nounset # error if variable is unset
 
-name=$(basename($pwd))
+name=$(basename $(pwd) )
 datetime=$(date +"%F-%H-%M-%S")
 
 mkdir -p log
 
 squeue --format "%Z" | grep $(pwd -P) || sbatch  <<EOF
 #!/usr/bin/env bash
-#SBATCH job-name=${name}
-#SBATCH nodes=2 --ntasks-per-node=28
-#SBATCH mem-per-cpu=1G
-#SBATCH partition=gpu
-#SBATCH qos=preempt
-#SBATCH gres=gpu:tesla:2
-#SBATCH output log/${name}_${datetime}.stdout
-#SBATCH error log/${name}_${datetime}.stderr
+#SBATCH --job-name=${name}
+#SBATCH --nodes=2 --ntasks-per-node=28
+#SBATCH --mem-per-cpu=1G
+#SBATCH --partition=gpu
+#SBATCH --qos=normal
+#SBATCH --gres=gpu:tesla:2
+#SBATCH --output log/${name}_${datetime}.stdout
+#SBATCH --error log/${name}_${datetime}.stderr
 set -o errexit
 set -o pipefail
 set -o verbose
 #load modules here
 module use /afm01/scratch/scmb/uqtlee10/modulefiles
 module load gromacs/2018.3-TLmod
-python deposition.py -i parameters.yml --debug --max-cores 28
-./submit_wiener
+python deposition.py -i parameters.yml --debug --max-cores 56 >> log/${name}_dep.log 2>&1
 EOF
