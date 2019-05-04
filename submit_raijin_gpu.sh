e5bb61bfa6a72f7de2f917ac7a3670d9819d68c8
Tom Lee <contact@tomlee.com.au>
2019-05-04 13:08:36 +1000

    Add ability to force resubmission in submit_raijin_gpu

--- submit_raijin_gpu.sh
+++ submit_raijin_gpu.sh
@@ -3,17 +3,23 @@
 set -o errexit   #
 set -o pipefail  # These lines cause bash to quit if any command has an error
 set -o verbose
-set -o nounset # error if variable is unset
 
 name=$(basename $(pwd) )
 datetime=$(date +"%F-%H-%M-%S")
 
 mkdir -p log
 
-ncpus=24
-ngpus=8
+ncpus=6
+ngpus=2
 
-qstat -f |tr -d " \t\n\r" | grep $(pwd) > /dev/null || qsub  <<EOF
+if [ "$1" == "force" ]
+then
+    qstat="echo x"
+else
+    qstat="qstat -f"
+fi
+
+$qstat | tr -d ' \t\n\r' | grep $(pwd) > /dev/null || qsub  <<EOF
 #!/usr/bin/env bash
 #PBS -S /bin/bash
 #PBS -P m72
@@ -22,7 +28,7 @@ qstat -f |tr -d " \t\n\r" | grep $(pwd) > /dev/null || qsub  <<EOF
 #PBS -l wd
 #PBS -l ncpus=${ncpus}
 #PBS -l ngpus=${ngpus}
-#PBS -l walltime=47:30:00
+#PBS -l walltime=12:00:00
 #PBS -l mem=24GB
 #PBS -l jobfs=100MB
 #PBS -o log/${name}_${datetime}.stdout
@@ -30,7 +36,21 @@ qstat -f |tr -d " \t\n\r" | grep $(pwd) > /dev/null || qsub  <<EOF
 set -o errexit
 set -o pipefail
 set -o verbose
+
+term_handler()
+{
+    echo "Restarting terminated job..."
+    ./submit_raijin_gpu.sh force
+    echo "Restarted terminated job."
+    exit -1
+}
+
+# uncomment to enable term handler
+# trap 'term_handler' SIGTERM
+
+export GMX_MAXBACKUP=-1
+
 #load modules here
 module load gromacs/2018.3-gpu
-python deposition.py --name $name -i parameters.yml --debug --max-cores ${ncpus} >> log/${name}_dep.log 2>&1
+python deposition.py --name $name -i parameters.yml --debug --max-cores ${ncpus}
 EOF
